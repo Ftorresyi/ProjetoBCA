12a9bf05a8016ef17081ed860c46f5067b2d43bd
unknown <Administrador@GAPGL070.gapgl.intraer>
2023-02-08 11:56:40 -0300

    Atualizando as ocorrencias de escritas diferentes das OMs

--- MarcaBCA.py
+++ MarcaBCA.py
@@ -22,65 +22,69 @@ nBCA=input('Digite o número do BCA que será lido: ')
 
 #LISTA DAS PALAVRAS QUE PRECISAM SER BUSCADAS NO PDF "bcadodia.PDF"
 OMs=[
-    'GAP GL','GAP-GL','GRUPAMENTO DE APOIO DO GALEÃO',
-    'PAMA GL','PAMA-GL','PARQUE DE MATERIAL AERONÁUTICO DO GALEÃO',  
-    'BAGL','BASE AÉREA DO GALEÃO','BAGL_ANTIGA'
-    'CMRJ','COLÉGIO MILITAR DO RIO DE JANEIRO',
-    '1 CJM','1° CJM','PRIMEIRA CIRCUNSCRIÇÃO JUDICIÁRIA MILITAR',
-    ' CEMAL','CENTRO DE MEDICINA AEROESPACIAL',    
-    'BINFAE GL','BINFAE-GL','BATALHÃO DE INFANTARIA DA AERONÁUTICA ESPECIAL DO GALEÃO',
-    'CCA RJ','CENTRO DE COMPUTAÇÃO DA AERONÁUTICA DO RIO DE JANEIRO',      
-    'PAGL','PREFEITURA DE AERONÁUTICA DO GALEÃO',
-    ' 3 ETA ',' 3° ETA ','TERCEIRO ETA',
-    ' GOP GL ',' GOP-GL ', 'GRUPO OPERACIONAL DO GALEÃO',
-    ' GLOG GL ','GLOG-GL','GRUPO LOGÍSTICO DO GALEÃO', 
+    ' GAP GL ',' GAP-GL ','GRUPAMENTO DE APOIO DO GALEÃO',' GAPGL ',
+    ' PAMA GL ',' PAMA-GL ','PARQUE DE MATERIAL AERONÁUTICO DO GALEÃO',' PAMA GL ' ,' PAMAGL ', 
+    ' BAGL ','BASE AÉREA DO GALEÃO','BAGL_ANTIGA'
+    ' CMRJ ','COLÉGIO MILITAR DO RIO DE JANEIRO',
+    ' 1 CJM ',' 1° CJM ','PRIMEIRA CIRCUNSCRIÇÃO JUDICIÁRIA MILITAR',' 1CJM ',' 1°CJM ',
+    ' CEMAL ','CENTRO DE MEDICINA AEROESPACIAL',    
+    ' BINFAE GL ','BINFAE-GL','BATALHÃO DE INFANTARIA DA AERONÁUTICA ESPECIAL DO GALEÃO',' BINFAEGL ',
+    ' CCA RJ ','CENTRO DE COMPUTAÇÃO DA AERONÁUTICA DO RIO DE JANEIRO',' CCARJ ',    
+    ' PAGL ','PREFEITURA DE AERONÁUTICA DO GALEÃO',
+    ' 3 ETA ',' 3° ETA ','TERCEIRO ETA',' 3ETA ',' 3°ETA '
+    ' GOP GL ',' GOP-GL ', 'GRUPO OPERACIONAL DO GALEÃO',' GOPGL ',' GOPGL ',
+    ' GLOG GL ',' GLOG-GL ','GRUPO LOGÍSTICO DO GALEÃO', ' GLOGGL ',
     ' MTAB','MISSÃO TÉCNICA AERONÁUTICA BRASILEIRA NA BOLÍVIA',
     ' DIRAP ','DIRETORIA DE ADMINISTRAÇÃO DO PESSOAL',
     ' DIRSA ','DIRETORIA DE SAÚDE DA AERONÁUTICA',
-    'CGABEG','CASA GERONTOLÓGICA DE AERONÁUTICA BRIGADEIRO EDUARDO GOMES',
-    'CBNB','COLEGIO BRIGADEIRO NEWTON BRAGA',
-    'SERIPA III','TERCEIRO SERVIÇO REGIONAL DE INVESTIGAÇÃO E PREVENÇÃO DE ACIDENTES AERONÁUTICOS',
+    ' CGABEG ','CASA GERONTOLÓGICA DE AERONÁUTICA BRIGADEIRO EDUARDO GOMES',
+    ' CBNB ','COLEGIO BRIGADEIRO NEWTON BRAGA',
+    ' SERIPA III ','TERCEIRO SERVIÇO REGIONAL DE INVESTIGAÇÃO E PREVENÇÃO DE ACIDENTES AERONÁUTICOS',' SERIPAIII ',
     ' ESG ','ESCOLA SUPERIOR DE GUERRA',
-    'PAMB RJ','PAMB-RJ''PARQUE DE MATERIAL BÉLICO DA AERONÁUTICA DO RIO DE JANEIRO',
-    'DTCEA-GL','DTCEA GL','DESTACAMENTO DE CONTROLE DO ESPAÇO AÉREO DO GALEÃO',
-    ' 1/2 GT',' 1°/2° GT','PRIMEIRO ESQUADRÃO DO SEGUNDO GRUPO DE TRANSPORTE',
-    ' 1 GCC','PRIMEIRO GRUPO DE COMUNICAÇÕES E CONTROLE',' 1° GCC',
-    ' 1/1 GT',' 1°/1° GT','PRIMEIRO ESQUADRÃO DO PRIMEIRO GRUPO DE TRANSPORTE',
-    ' 2/2 GT',' 2°/2° GT','SEGUNDO ESQUADRÃO DO SEGUNDO GRUPO DE TRANSPORTE',
-    'LAQFA','LABORATÓRIO QUÍMICO-FARMACÊUTICO DA AERONÁUTICA',
-    'CTLA','CENTRO DE TRANSPORTE LOGÍSTICO DA AERONÁUTICA',
-    'ALA 11', 'BASE AÉREA DO GALEÃO'
-    'CIMAER','CENTRO INTEGRADO DE METEOROLOGIA AERONÁUTICA',
+    ' PAMB RJ ',' PAMB-RJ ','PARQUE DE MATERIAL BÉLICO DA AERONÁUTICA DO RIO DE JANEIRO',' PAMBRJ ',
+    ' DTCEA-GL ',' DTCEA GL ','DESTACAMENTO DE CONTROLE DO ESPAÇO AÉREO DO GALEÃO',' DTCEAGL ',
+    ' 1/2 GT ',' 1°/2° GT','PRIMEIRO ESQUADRÃO DO SEGUNDO GRUPO DE TRANSPORTE',' 1/2GT',' 1°/2°GT','1º/2º GT','1º/2ºGT'
+    ' 1 GCC ','PRIMEIRO GRUPO DE COMUNICAÇÕES E CONTROLE',' 1° GCC ',' 1GCC ',' 1°GCC ','1ºGCC','1º GCC'
+    ' 1/1 GT ',' 1°/1° GT','PRIMEIRO ESQUADRÃO DO PRIMEIRO GRUPO DE TRANSPORTE',' 1/1GT ',' 1°/1°GT','1º/1º GT','1º/1ºGT'
+    ' 2/2 GT ',' 2°/2° GT ','SEGUNDO ESQUADRÃO DO SEGUNDO GRUPO DE TRANSPORTE',' 2°/2°GT ',' 2/2GT ','2º/2º GT','2º/2ºGT'
+    ' LAQFA ','LABORATÓRIO QUÍMICO-FARMACÊUTICO DA AERONÁUTICA',
+    ' CTLA ','CENTRO DE TRANSPORTE LOGÍSTICO DA AERONÁUTICA',
+    ' ALA 11 ', 'BASE AÉREA DO GALEÃO', ' ALA11 ',
+    ' CIMAER ','CENTRO INTEGRADO DE METEOROLOGIA DA AERONÁUTICA',
     ' CAE ','CENTRO DE AQUISIÇÕES ESPECÍFICAS',
-    'COPE-S','CENTRO DE OPERAÇÕES ESPACIAIS SECUNDÁRIO'
-    'GSD-GL','GRUPO DE SEGURANÇA E DEFESA DO GALEÃO',
-    'HFAG', 'HOSPITAL DE FORÇA AÉREA DO GALEÃO', 
-    'MOVIMENTAÇÃO', 'PROMOÇÃO', 'COGITAÇÃO']
+    ' COPE-S ','CENTRO DE OPERAÇÕES ESPACIAIS SECUNDÁRIO', ' COPE S ',
+    ' GSD-GL ','GRUPO DE SEGURANÇA E DEFESA DO GALEÃO',' GSDGL ',
+    ' HFAG ', 'HOSPITAL DE FORÇA AÉREA DO GALEÃO']
 
 #LISTA DAS PALAVRAS QUE DEVERÃO SER DESMARCADAS:
-PalavrasChave=[' PORTARIA DIRAP ', ' da PORTARIA ', '\tPortaria Dirap n', '\nPortaria DIRAP n°', 'Subdiretor Interino de Pessoal Militar da Dirap', 
-'Subdiretor Interino de Pessoal Civil da Dirap']
+PalavrasChave=[' PORTARIA DIRAP ', ' DA PORTARIA ','\tPORTARIA DIRAP N', 'PORTARIA DIRAP N°', 'SUBDIRETOR INTERINO DE PESSOAL MILITAR DA DIRAP', 
+'SUBDIRETOR INTERINO DE PESSOAL CIVIL DA DIRAP', ' NOTA DIRAP ', ' PORTARIA  DIRAP ','^PORTARIA DIRAP Nº']
 
+sumannot = 0
 def MarcaOMsApoiadas(*args):
   BcadoDia = fitz.open(*args)
-  
+  #sumannot = 0
+  global sumannot
   for page in BcadoDia: #loop para busca pagina a pagina dentro do BcadoDia
     for i in OMs:   #loop busca OM por OM da lista OMs em cada pagina do BcadoDia
-      text_instances = page.search_for(i)  # text_instances recebe a busca da lista de OMs em todas as paginas  
+      text_instances = page.search_for(i.upper())  # text_instances recebe a busca da lista de OMs em todas as paginas
       if text_instances != []:  #Se a lista de OMs nao for vazia    
         for inst in text_instances: #Percorre a lista de palavras achadas uma a uma 
           highlight = page.addHighlightAnnot(inst) #marca a palavra da lista de achadas
           print('Foi encontrada a palavra: ', highlight) #imprime a palavra marcada que eh um objeto da classe Fitz
-    
+          sumannot = sumannot + 1
+  print('Foram encontradas: ', sumannot, ' anotações no total')
   BcadoDia.save("bca_do_dia_marcado.pdf")
         
        
 #by: ALAN KLINGER =================================================================================
 #o jeito mais fácil foi fazer uma função semelhante a sua
 #OBS: Isso está desmarcando os (DIRAP), não sei se era esse o objetivo
+nannot=0
 def removeHighlightv2(pdf_marcado, palavras_para_desmarcar):
   BcadoDia = fitz.open(pdf_marcado)
-  nannot=0
+  #nannot=0
+  global nannot
   for page in BcadoDia:
     #procuro pelas palavras que devem ser desmarcadas
     for palavra in palavras_para_desmarcar:
@@ -93,16 +97,15 @@ def removeHighlightv2(pdf_marcado, palavras_para_desmarcar):
           #e verificar se há intercessão entre a anotação e aquilo que não deveria estar anotado
           for annot in page.annots():
             if (inst.intersect(annot.rect)):
-              print("Remove anotacao ", palavra ," pg", page.number)
+              #print("Remove anotacao ", palavra ," pg", page.number)
               page.delete_annot(annot)
               nannot=nannot+1
   print('Foram removidas ', nannot, ' anotações')
   BcadoDia.save("Bca-"+nBCA+"_Marcado-"+data_atual+".pdf")
   
-
-
 pdfmarcado=MarcaOMsApoiadas(input("Digite o nome do arquivo BCA que será lido, sem a extensão: ")+".pdf")
 removeHighlightv2('bca_do_dia_marcado.pdf', PalavrasChave)
+print('O Total de palavras encontradas para transcrição são: ', sumannot-nannot)
 os.remove("bca_do_dia_marcado.pdf")
 
 
